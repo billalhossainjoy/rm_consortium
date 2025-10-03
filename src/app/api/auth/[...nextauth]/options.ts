2f7b9cbfdd766d4f838c6b4ede1ba81f59347a2a
Billal Hossain <billalhossain.bhj@gmail.com>
2025-10-03 17:03:11 +0600

    fix auth

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -5,10 +5,29 @@ const authOptions = {
   providers: [
     // !!! Should be stored in .env file.
     GoogleProvider({
-      clientId: `1041339102270-e1fpe2b6v6u1didfndh7jkjmpcashs4f.apps.googleusercontent.com`,
-      clientSecret: `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`,
+      clientId: process.env.CLIENT_ID!,
+      clientSecret: process.env.CLIENT_SECRET!,
     }),
   ],
+    callbacks: {
+      async signIn({user}) {
+          console.log(user)
+          const adminEmail =process.env.ADMIN_EMAIL;
+
+      if (user?.email === adminEmail) {
+        return true; // allow login
+      }
+
+      return false;
+      },
+
+      async redirect({ _, baseUrl}){
+          return `${baseUrl}/admin`;
+      }
+    },
+    pages: {
+      error: "/"
+    },
   secret: `UItTuD1HcGXIj8ZfHUswhYdNd40Lc325R8VlxQPUoR0=`,
 };
 
